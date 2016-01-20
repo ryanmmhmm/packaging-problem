f67467ed571973d167f1a7d9cefec0c5180b397d
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-20 14:33:08 -0500

    setting up instances of Quote object in test and testing that attributes are being defined correctly

--- packaging/test_packaging.rb
+++ packaging/test_packaging.rb
@@ -3,6 +3,16 @@ require 'packaging.rb'
 
 class TestPackaging < MiniTest::Test
 
+  def setup
+    @input1 = Quote.new("$1299.99","3 people","food")
+    @input2 = Quote.new("$5432.00","1 person","drugs")
+    @input3 = Quote.new("$12456.95","4 people","books")
+  end
 
+  def test_that_input_has_matching_attr
+    assert_equal("$1299.99", @input1.cost)
+    assert_equal("3 people", @input1.people)
+    assert_equal("food", @input1.product_type)
+  end
 
 end
