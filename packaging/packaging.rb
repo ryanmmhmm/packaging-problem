3d6538fac3559271fcb0df8834ad8b0890d51a31
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-21 17:37:00 -0500

    Price now accepts Quote as input rather than association, changed self references to instance variables, adding calculation for product_markup

--- packaging/packaging.rb
+++ packaging/packaging.rb
@@ -2,11 +2,10 @@ class Quote
   attr_accessor :cost, :people, :product_type
   attr_reader :price
 
-  def initialize(cost, people, product_type, price = Price.new)
+  def initialize(cost, people, product_type)
     @cost = cost
     @people = people
     @product_type = product_type
-    @price = price
 
     self.format_attributes
   end
@@ -20,10 +19,10 @@ class Quote
   end
 
   def format_cost
-    if self.cost.is_a?(String)
-        self.cost = remove_dollar_sign(self.cost)
+    if @cost.is_a?(String)
+        @cost = remove_dollar_sign(@cost)
     end
-    self.cost = self.cost.to_f
+    @cost = @cost.to_f
   end
 
   def remove_letters(input)
@@ -37,31 +36,46 @@ class Quote
   end
 
   def format_people
-    self.people = remove_letters(self.people)
-    self.people = self.people.to_i
+    @people = remove_letters(@people)
+    @people = @people.to_i
   end
 
   def format_product_type
-    self.product_type.tr!(' ','')
-    self.product_type.downcase!
-    return self.product_type
+    @product_type.tr!(' ','')
+    @product_type.downcase!
+    @product_type
   end
 
   def format_attributes
-    self.cost = self.format_cost
-    self.people = self.format_people
-    self.product_type = self.format_product_type
+    @cost = self.format_cost
+    @people = self.format_people
+    @product_type = self.format_product_type
   end
 end
 
 
 class Price
 
-  attr_reader :base_markup, :manpower_markup
+  attr_reader :quote, :base_markup, :manpower_markup, :product_markup, :total_markup
 
-  def initialize
+  def initialize(quote)
+    @quote = quote
     @base_markup = 0.05
     @manpower_markup = 0.012
+    @product_markup = 0
+  end
+
+  def calculate_product_markup(product_type)
+    case product_type
+    when 'drugs'
+      @product_markup = 0.075
+    when 'food'
+      @product_markup = 0.13
+    when 'electronics'
+      @product_markup = 0.02
+    else
+      @product_markup = 0
+    end
   end
 
 end
