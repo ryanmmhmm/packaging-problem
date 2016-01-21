4ea8483e89cdec71910bc30ef13db63d14b4ebd1
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-21 17:43:39 -0500

    adding tests for other input product types

--- packaging/test_packaging.rb
+++ packaging/test_packaging.rb
@@ -44,4 +44,12 @@ class TestPackaging < MiniTest::Test
     assert_equal(0.13, @output1.calculate_product_markup(@output1.quote.product_type))
   end
 
+  def test_that_drugs_has_proper_markup
+    assert_equal(0.075, @output2.calculate_product_markup(@output2.quote.product_type))
+  end
+
+  def test_that_books_has_proper_markup
+    assert_equal(0.00, @output3.calculate_product_markup(@output3.quote.product_type))
+  end
+
 end
